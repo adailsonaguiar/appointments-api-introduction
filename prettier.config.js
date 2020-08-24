119a1bbe56db43ee54f768dfac54c0724b3ae107
unknown <adailsonacj@live.com>
2020-08-23 22:19:36 -0300

    first commit

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  arrowParens: 'avoid',
+  tableWidth: 2
+}
---
+++ src/models/Appointments.ts
@@ -0,0 +1,16 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { uuid } from 'uuidv4';
+
+export default class Appointment {
+  id: string;
+
+  provider: string;
+
+  date: Date;
+
+  constructor({ provider, date }: Omit<Appointment, 'id'>) {
+    this.provider = provider;
+    this.date = date;
+    this.id = uuid();
+  }
+}
